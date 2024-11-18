454983746866e32886846afdaeaa566a7363db30
BraumAce <1693717911@qq.com>
2024-11-18 23:41:19 +0800

    add：QuestionMapper

---
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/mapper/QuestionMapper.java
@@ -0,0 +1,17 @@
+package com.yuan.mianshiba.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuan.mianshiba.model.entity.Question;
+
+/**
+* @author BraumAce
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @Entity generator.domain.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
