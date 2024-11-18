8023e072dff7b98301308b9dd605f51c8a16a945
BraumAce <1693717911@qq.com>
2024-11-18 23:41:51 +0800

    update：UserMapper

--- mianshiba-backend/src/main/java/com/yuan/mianshiba/mapper/UserMapper.java
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/mapper/UserMapper.java
@@ -4,10 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yuan.mianshiba.model.entity.User;
 
 /**
- * 用户数据库操作
- *
- * @author BraumAce
- */
+* @author BraumAce
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @Entity generator.domain.User
+*/
 public interface UserMapper extends BaseMapper<User> {
 
 }
