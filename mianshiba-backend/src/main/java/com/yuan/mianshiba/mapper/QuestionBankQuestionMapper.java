6877bdb1a13cc2526645eb90b39927afbd990a68
BraumAce <1693717911@qq.com>
2024-11-18 23:41:28 +0800

    add：QuestionBankQuestionMapper

---
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,17 @@
+package com.yuan.mianshiba.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuan.mianshiba.model.entity.QuestionBankQuestion;
+
+/**
+* @author BraumAce
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @Entity generator.domain.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
