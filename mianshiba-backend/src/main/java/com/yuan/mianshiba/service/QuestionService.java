5841bb9c21e0ace03880ae85f9dc686b35952896
BraumAce <1693717911@qq.com>
2024-11-20 21:15:46 +0800

    add：题目服务

---
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/service/QuestionService.java
@@ -0,0 +1,52 @@
+package com.yuan.mianshiba.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuan.mianshiba.model.dto.question.QuestionQueryRequest;
+import com.yuan.mianshiba.model.entity.Question;
+import com.yuan.mianshiba.model.vo.QuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 题目服务
+ *
+ * @author BraumAce
+ */
+public interface QuestionService extends IService<Question> {
+
+    /**
+     * 校验数据
+     *
+     * @param question
+     * @param add 对创建的数据进行校验
+     */
+    void validQuestion(Question question, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest);
+
+    /**
+     * 获取题目封装
+     *
+     * @param question
+     * @param request
+     * @return
+     */
+    QuestionVO getQuestionVO(Question question, HttpServletRequest request);
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request);
+}
