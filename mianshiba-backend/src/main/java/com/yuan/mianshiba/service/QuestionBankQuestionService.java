3309c6dd36cf15f301f3905c012bbea9e3276f72
BraumAce <1693717911@qq.com>
2024-11-20 21:16:23 +0800

    add：题库题目关联服务

---
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/service/QuestionBankQuestionService.java
@@ -0,0 +1,52 @@
+package com.yuan.mianshiba.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuan.mianshiba.model.dto.questionBankQuestion.QuestionBankQuestionQueryRequest;
+import com.yuan.mianshiba.model.entity.QuestionBankQuestion;
+import com.yuan.mianshiba.model.vo.QuestionBankQuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 题库题目关联服务实现
+ *
+ * @author BraumAce
+ */
+public interface QuestionBankQuestionService extends IService<QuestionBankQuestion> {
+
+    /**
+     * 校验数据
+     *
+     * @param questionBankQuestion
+     * @param add 对创建的数据进行校验
+     */
+    void validQuestionBankQuestion(QuestionBankQuestion questionBankQuestion, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionBankQuestionQueryRequest
+     * @return
+     */
+    QueryWrapper<QuestionBankQuestion> getQueryWrapper(QuestionBankQuestionQueryRequest questionBankQuestionQueryRequest);
+
+    /**
+     * 获取题库封装
+     *
+     * @param questionBankQuestion
+     * @param request
+     * @return
+     */
+    QuestionBankQuestionVO getQuestionBankQuestionVO(QuestionBankQuestion questionBankQuestion, HttpServletRequest request);
+
+    /**
+     * 分页获取题库封装
+     *
+     * @param questionBankQuestionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionBankQuestionVO> getQuestionBankQuestionVOPage(Page<QuestionBankQuestion> questionBankQuestionPage, HttpServletRequest request);
+}
