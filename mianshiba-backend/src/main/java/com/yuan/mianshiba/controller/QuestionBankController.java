8830afae9fc0d39523eaadf8e12fc1c857be394a
BraumAce <1693717911@qq.com>
2024-11-23 23:40:38 +0800

    update：根据id获取题库详情

--- mianshiba-backend/src/main/java/com/yuan/mianshiba/controller/QuestionBankController.java
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/controller/QuestionBankController.java
@@ -9,14 +9,17 @@ import com.yuan.mianshiba.common.ResultUtils;
 import com.yuan.mianshiba.constant.UserConstant;
 import com.yuan.mianshiba.exception.BusinessException;
 import com.yuan.mianshiba.exception.ThrowUtils;
+import com.yuan.mianshiba.model.dto.question.QuestionQueryRequest;
 import com.yuan.mianshiba.model.dto.questionBank.QuestionBankAddRequest;
 import com.yuan.mianshiba.model.dto.questionBank.QuestionBankEditRequest;
 import com.yuan.mianshiba.model.dto.questionBank.QuestionBankQueryRequest;
 import com.yuan.mianshiba.model.dto.questionBank.QuestionBankUpdateRequest;
+import com.yuan.mianshiba.model.entity.Question;
 import com.yuan.mianshiba.model.entity.QuestionBank;
 import com.yuan.mianshiba.model.entity.User;
 import com.yuan.mianshiba.model.vo.QuestionBankVO;
 import com.yuan.mianshiba.service.QuestionBankService;
+import com.yuan.mianshiba.service.QuestionService;
 import com.yuan.mianshiba.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -39,18 +42,22 @@ public class QuestionBankController {
     private QuestionBankService questionBankService;
 
     @Resource
+    private QuestionService questionService;
+
+    @Resource
     private UserService userService;
 
     // region 增删改查
 
     /**
-     * 创建题库
+     * 创建题库（仅管理员可用）
      *
      * @param questionBankAddRequest
      * @param request
      * @return
      */
     @PostMapping("/add")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Long> addQuestionBank(@RequestBody QuestionBankAddRequest questionBankAddRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(questionBankAddRequest == null, ErrorCode.PARAMS_ERROR);
         // todo 在此处将实体类和 DTO 进行转换
@@ -126,17 +133,30 @@ public class QuestionBankController {
     /**
      * 根据 id 获取题库（封装类）
      *
-     * @param id
+     * @param questionBankQueryRequest
+     * @param request
      * @return
      */
     @GetMapping("/get/vo")
-    public BaseResponse<QuestionBankVO> getQuestionBankVOById(long id, HttpServletRequest request) {
+    public BaseResponse<QuestionBankVO> getQuestionBankVOById(QuestionBankQueryRequest questionBankQueryRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(questionBankQueryRequest == null, ErrorCode.PARAMS_ERROR);
+        Long id = questionBankQueryRequest.getId();
         ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
         // 查询数据库
         QuestionBank questionBank = questionBankService.getById(id);
         ThrowUtils.throwIf(questionBank == null, ErrorCode.NOT_FOUND_ERROR);
+        // 查询题库封装类
+        QuestionBankVO questionBankVO = questionBankService.getQuestionBankVO(questionBank, request);
+        // 是否要关联查询该题库下的题目列表
+        boolean needQueryQuestionList = questionBankQueryRequest.isNeedQueryQuestionList();
+        if (needQueryQuestionList) {
+            QuestionQueryRequest questionQueryRequest = new QuestionQueryRequest();
+            questionQueryRequest.setQuestionBankId(id);
+            Page<Question> questionPage = questionService.listQuestionByPage(questionQueryRequest);
+            questionBankVO.setQuestionPage(questionPage);
+        }
         // 获取封装类
-        return ResultUtils.success(questionBankService.getQuestionBankVO(questionBank, request));
+        return ResultUtils.success(questionBankVO);
     }
 
     /**
@@ -203,13 +223,14 @@ public class QuestionBankController {
     }
 
     /**
-     * 编辑题库（给用户使用）
+     * 编辑题库（仅管理员可用）
      *
      * @param questionBankEditRequest
      * @param request
      * @return
      */
     @PostMapping("/edit")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Boolean> editQuestionBank(@RequestBody QuestionBankEditRequest questionBankEditRequest, HttpServletRequest request) {
         if (questionBankEditRequest == null || questionBankEditRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
