7ec98633972d95f6b385d7f076bbc94fd23d8b88
BraumAce <1693717911@qq.com>
2025-02-02 23:12:47 +0800

    [feat] Global initialization logic

--- mianshiba-frontend/src/app/layout.tsx
+++ mianshiba-frontend/src/app/layout.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import React, { useCallback, useEffect } from "react";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import BasicLayout from "@/layouts/BasicLayout";
 import "./globals.css";
+
+/**
+ * 全局初始化逻辑
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<
+  Readonly<{
+    children: React.ReactNode;
+  }>
+> = ({ children }) => {
+  /**
+   * 全局初始化函数
+   */
+  const doInit = useCallback(() => {
+    console.log("hello！这里是面试吧！")
+  }, []);
+
+  // 只执行一次
+  useEffect(() => {
+    doInit();
+  }, [doInit]);
+
+  return children;
+};
 
 export default function RootLayout({
   children,
@@ -12,7 +38,9 @@ export default function RootLayout({
     <html lang="zh">
       <body>
         <AntdRegistry>
-          <BasicLayout>{children}</BasicLayout>
+          <InitLayout>
+            <BasicLayout>{children}</BasicLayout>
+          </InitLayout>
         </AntdRegistry>
       </body>
     </html>
