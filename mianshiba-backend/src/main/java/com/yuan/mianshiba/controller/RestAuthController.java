431b725b0f1ccf58e66cc344d563bfa064702e5e
BraumAce <1693717911@qq.com>
2025-04-26 23:47:48 +0800

    [feature] authLogin init

---
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/controller/RestAuthController.java
@@ -0,0 +1,40 @@
+package com.yuan.mianshiba.controller;
+
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.request.AuthGithubRequest;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author liuguang
+ */
+@RestController
+@RequestMapping("/oauth")
+public class RestAuthController {
+
+    @RequestMapping("/render")
+    public void renderAuth(HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = getAuthRequest();
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @RequestMapping("/callback")
+    public Object login(AuthCallback callback) {
+        AuthRequest authRequest = getAuthRequest();
+        return authRequest.login(callback);
+    }
+
+    private AuthRequest getAuthRequest() {
+        return new AuthGithubRequest(AuthConfig.builder()
+                .clientId("Client ID")
+                .clientSecret("Client Secret")
+                .redirectUri("应用回调地址")
+                .build());
+    }
+}
