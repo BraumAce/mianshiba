f814d2f1b0a3f403904dfcf32b2a835060849263
BraumAce <1693717911@qq.com>
2025-05-28 21:52:00 +0800

    [feature] 更新三方授权接口（backend）

--- mianshiba-backend/src/main/java/com/yuan/mianshiba/controller/RestAuthController.java
+++ mianshiba-backend/src/main/java/com/yuan/mianshiba/controller/RestAuthController.java
@@ -1,40 +1,121 @@
 package com.yuan.mianshiba.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.xkcoding.http.config.HttpConfig;
+import lombok.extern.slf4j.Slf4j;
 import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.enums.scope.AuthGithubScope;
+import me.zhyd.oauth.enums.scope.AuthGoogleScope;
+import me.zhyd.oauth.exception.AuthException;
 import me.zhyd.oauth.model.AuthCallback;
-import me.zhyd.oauth.request.AuthGithubRequest;
-import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.model.AuthUser;
+import me.zhyd.oauth.request.*;
+import me.zhyd.oauth.utils.AuthScopeUtils;
 import me.zhyd.oauth.utils.AuthStateUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
+ * 三方授权登录接口
+ *
  * @author liuguang
  */
+@Slf4j
 @RestController
 @RequestMapping("/oauth")
 public class RestAuthController {
 
-    @RequestMapping("/render")
-    public void renderAuth(HttpServletResponse response) throws IOException {
-        AuthRequest authRequest = getAuthRequest();
-        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+//    @RequestMapping("/callback")
+//    public Object login(AuthCallback callback) {
+//        AuthRequest authRequest = getAuthRequest();
+//        return authRequest.login(callback);
+//    }
+
+    @GetMapping("/render/{source}")
+    public void renderAuth(@PathVariable("source") String source, HttpServletResponse response) throws IOException {
+        log.info("进入render：{}", source);
+        AuthRequest authRequest = getAuthRequest(source);
+        String authorizeUrl = authRequest.authorize(AuthStateUtils.createState());
+        log.info(authorizeUrl);
+        response.sendRedirect(authorizeUrl);
     }
 
-    @RequestMapping("/callback")
-    public Object login(AuthCallback callback) {
-        AuthRequest authRequest = getAuthRequest();
-        return authRequest.login(callback);
+    @RequestMapping("/callback/{source}")
+    public ModelAndView login(@PathVariable("source") String source, AuthCallback callback, HttpServletRequest request) {
+        log.info("进入callback：{} callback params：{}", source, JSONObject.toJSONString(callback));
+        AuthRequest authRequest = getAuthRequest(source);
+        AuthResponse<AuthUser> response = authRequest.login(callback);
+        log.info(JSONObject.toJSONString(response));
+
+        if (response.ok()) {
+//            userService.save(response.getData());
+//            return authRequest.login(callback);
+            return new ModelAndView("redirect:/users");
+        }
+
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("errorMsg", response.getMsg());
+
+        return new ModelAndView("error", map);
     }
 
-    private AuthRequest getAuthRequest() {
-        return new AuthGithubRequest(AuthConfig.builder()
-                .clientId("Client ID")
-                .clientSecret("Client Secret")
-                .redirectUri("应用回调地址")
-                .build());
+    /**
+     * 根据具体的授权来源，获取授权请求工具类
+     *
+     * @param source
+     * @return
+     */
+    private AuthRequest getAuthRequest(String source) {
+        AuthRequest authRequest = null;
+        switch (source.toLowerCase()) {
+            case "qq":
+                authRequest = new AuthQqRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("http://localhost:8443/oauth/callback/qq")
+                        .build());
+                break;
+            case "github":
+                authRequest = new AuthGithubRequest(AuthConfig.builder()
+                        .clientId("Iv23liDc1CF8A4QE4Q0y")
+                        .clientSecret("d3223ab226d9edb0fcd9ca12ef96373da1aea837")
+                        .redirectUri("http://localhost:8443/oauth/callback/github")
+                        .scopes(AuthScopeUtils.getScopes(AuthGithubScope.values()))
+                        // 针对国外平台配置代理
+                        .httpConfig(HttpConfig.builder()
+                                .timeout(15000)
+                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890)))
+                                .build())
+                        .build());
+                break;
+            case "google":
+                authRequest = new AuthGoogleRequest(AuthConfig.builder()
+                        .clientId("868027239116-neei8f6t31or27aveamv3luc619v0hrd.apps.googleusercontent.com")
+                        .clientSecret("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")
+                        .redirectUri("http://localhost:8443/oauth/callback/google")
+                        .scopes(AuthScopeUtils.getScopes(AuthGoogleScope.USER_EMAIL, AuthGoogleScope.USER_PROFILE, AuthGoogleScope.USER_OPENID))
+                        // 针对国外平台配置代理
+                        .httpConfig(HttpConfig.builder()
+                                .timeout(15000)
+                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890)))
+                                .build())
+                        .build());
+                break;
+            default:
+                break;
+        }
+        if (null == authRequest) {
+            throw new AuthException("未获取到有效的Auth配置");
+        }
+        return authRequest;
     }
 }
